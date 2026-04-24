0f29c81a2a5ac4ceb41f3843e79e22ef2cb55459
Om Bidikar <ombidikar72@gmail.com>
2026-04-24 23:23:30 +0530

    Add files via upload

---
+++ ECU1/src/ecu1_main.c
@@ -0,0 +1,49 @@
+/*
+ * File:   main.c
+ * Author: Ajinkya
+ *
+ * Created on October 30, 2025, 11:48 AM
+ */
+
+
+#include "adc.h"
+#include "can.h"
+#include "ecu1_sensor.h"
+#include "msg_id.h"
+#include "uart.h"
+#include "ssd.h"
+#include<stdio.h>
+#define _XTAL_FREQ 2000000
+void init_config(void) {
+    init_adc();
+    init_digital_keypad();
+    init_uart();
+    init_can();
+//    init_ssd();
+}
+int main()
+{
+    //Call the functions
+    init_config();
+    uint16_t speed;
+    uint8_t len;
+    unsigned char gear;
+    char buff[4],data[4];
+    while(1) {
+        speed = get_speed();
+        gear =  get_gear_pos();
+        if(gear == 'N' && speed > 0) speed = 0;
+        else if(gear == '1' && speed > 20) speed = 20;
+        else if(gear == '2' && speed > 40) speed = 40;
+        else if(gear == '3' && speed > 60) speed = 60;
+        else if(gear == '4' && speed > 80) speed = 80;
+        if(speed >= 0 && speed <= 9) len = 1;
+        else if(speed >= 10 && speed <= 99) len = 2;
+        else if(speed == 100) len = 3;
+        sprintf(buff, "%hd", speed);
+        can_transmit(SPEED_MSG_ID, buff, len);
+        __delay_ms(80);
+        can_transmit(GEAR_MSG_ID, &gear, 1);
+        __delay_ms(80);
+    }
+}
---
+++ ECU1/src/ecu1_sensor.c
@@ -0,0 +1,46 @@
+/*
+ * File:   ecu1_sensors.c
+ * Author: Ajinkya
+ *
+ * Created on October 30, 2025, 11:57 AM
+ */
+
+
+#include "ecu1_sensor.h"
+#include "adc.h"
+#include "can.h"
+#include "msg_id.h"
+#include "uart.h"
+uint16_t get_speed()
+{
+    return (uint16_t)(read_adc(CHANNEL4) / 10.23);
+    // Implement the speed function
+}
+
+unsigned char get_gear_pos()
+{
+    static unsigned char gear = 'N';
+    // Implement the gear function
+    unsigned char key;
+    key = read_digital_keypad(STATE_CHANGE);
+    if(key == SWITCH1) {
+        // increment gear
+        if(gear == 'R' || gear == 'N')
+            gear = '1';
+        else if(gear < '5')
+            gear++;
+    }
+    else if(key == SWITCH2) {
+        // decrement gear
+        if(gear > '1' && gear != 'R' && gear != 'N')
+            gear--;
+    }
+    else if(key == SWITCH3)
+        // reverse
+        gear = 'R';
+    else if(key == SWITCH4)
+        // nuatral
+        gear = 'N';
+    
+    return gear;
+}
